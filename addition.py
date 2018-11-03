cd8dc8d81979b3a06bd317d90349b99cdcdf38b7
hrishikeshsawant <44426691+hrishikeshsawant@users.noreply.github.com>
2018-11-03 14:07:32 +0530

    Add files via upload

---
+++ addition.py
@@ -0,0 +1,6 @@
+Python 3.6.6 (v3.6.6:4cf1f54eb7, Jun 27 2018, 03:37:03) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> inp1=6
+>>> inp2=9
+>>> add=input(inp1+inp2);
+15
